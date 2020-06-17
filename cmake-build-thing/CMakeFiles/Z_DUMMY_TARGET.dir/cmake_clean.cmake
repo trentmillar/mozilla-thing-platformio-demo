6e4ec4d914b95c42b79d8337b3a555ce5d7bb0ac
trentmillar <trent.millar@gmail.com>
2020-06-16 23:10:11 -0600

    initial commit

---
+++ cmake-build-thing/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.c.o"
+  "Z_DUMMY_TARGET.pdb"
+  "Z_DUMMY_TARGET"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+}
